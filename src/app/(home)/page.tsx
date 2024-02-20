4b2514e764e6b11a70a9e210fec778022229d8ee
Wander Amaral <wanderamaral013@gmail.com>
2024-02-20 16:38:18 -0300

    feat: created message welcome

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,18 @@
+import { ptBR } from "date-fns/locale";
 import Header from "../_components/header";
+import { format } from "date-fns";
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <div className="pt-5 px-5">
+        <h1 className="text-xl font-bold">Olá, Miguél</h1>
+        <p className=" capitalize text-sm">
+          {format(new Date(), "EEEE, d 'de'  MMMM", { locale: ptBR })}
+        </p>
+      </div>
     </div>
   );
 }
