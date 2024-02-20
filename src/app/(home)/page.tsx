38025650e45d8195b53397635e4a504f40b64704
Wander Amaral <wanderamaral013@gmail.com>
2024-02-20 16:50:48 -0300

    feat add commit

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,17 +1,21 @@
 import { ptBR } from "date-fns/locale";
 import Header from "../_components/header";
 import { format } from "date-fns";
+import Search from "./_components/search";
 
 export default function Home() {
   return (
     <div>
       <Header />
 
-      <div className="pt-5 px-5">
+      <div className="py-6 px-5">
         <h1 className="text-xl font-bold">Olá, Miguél</h1>
         <p className=" capitalize text-sm">
           {format(new Date(), "EEEE, d 'de'  MMMM", { locale: ptBR })}
         </p>
+      </div>
+      <div className="px-5">
+        <Search />
       </div>
     </div>
   );
