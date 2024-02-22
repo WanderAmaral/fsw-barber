57d67568d902a2fc1fb0541a1bb8e9a9ee67e94b
Wander Amaral <wanderamaral013@gmail.com>
2024-02-22 17:00:50 -0300

    feat: commit

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,16 +1,15 @@
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
-import { MenuIcon} from "lucide-react";
+import { MenuIcon } from "lucide-react";
+import MenuSheet from "./menu-sheet";
 
 const Header = () => {
   return (
     <Card>
       <CardContent className="py-6 px-5 flex items-center justify-between">
         <Image src={"/Logo.png"} alt="logo" height={22} width={130} />
-        <Button variant="outline" size='icon' className="h-8 w-8">
-            <MenuIcon size={18}/>
-        </Button>
+        <MenuSheet />
       </CardContent>
     </Card>
   );
