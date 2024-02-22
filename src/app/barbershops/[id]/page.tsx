0034702f36b1217d87fe622189c6331355edc795
Wander Amaral <wanderamaral013@gmail.com>
2024-02-22 17:54:17 -0300

    feat comit

--- src/app/barbershops/[id]/page.tsx
+++ src/app/barbershops/[id]/page.tsx
@@ -1,8 +1,5 @@
-import MenuSheet from "@/app/_components/menu-sheet";
-import { Button } from "@/app/_components/ui/button";
 import { db } from "@/app/_lib/prisma";
-import { ChevronLeftIcon, MapPinIcon, StarIcon } from "lucide-react";
-import Image from "next/image";
+import BarbershopInfo from "./_components/barbershop-info";
 
 interface BarbershopDetailsPageProps {
   params: {
@@ -28,35 +25,9 @@ const BarbershopDetailsPage = async ({
     return null;
   }
   return (
-    <div>
-      <div className="h-[250px] w-full relative">
-        <div className="p-6 flex justify-between z-50 absolute w-full">
-          <Button variant={"outline"} size={"icon"} className="">
-            <ChevronLeftIcon />
-          </Button>
-          <div className="h-full">
-            <MenuSheet />
-          </div>
-        </div>
-        <Image
-          src={barbershop.imageUrl}
-          alt={barbershop.name}
-          fill
-          style={{ objectFit: "cover" }}
-        />
-      </div>
-      <div className="px-5 py-3 ">
-        <h1 className="text-xl font-bold">{barbershop.name}</h1>
-        <div className="flex items-center gap-1">
-          <MapPinIcon className=" text-primary" size={16} />
-          <p className="text-sm">{barbershop.address}</p>
-        </div>
-        <div className="flex items-center gap-1">
-          <StarIcon className=" text-primary" size={16} />
-          <p className="text-sm"><span>5,0 (889 vizualizações)</span></p>
-        </div>
-      </div>
-    </div>
+    <>
+      <BarbershopInfo barbershop={barbershop} />
+    </>
   );
 };
 
