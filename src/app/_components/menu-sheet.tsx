6cef18e9208c5ac31207ce8259658cffbe1681fa
Wander Amaral <wanderamaral013@gmail.com>
2024-03-10 14:29:31 -0300

    feat: completed menu side bar

--- src/app/_components/menu-sheet.tsx
+++ src/app/_components/menu-sheet.tsx
@@ -1,10 +1,11 @@
 "use client";
-import { LogInIcon, MenuIcon } from "lucide-react";
+import { CalendarIcon, HomeIcon, LogInIcon, MenuIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { Sheet, SheetContent, SheetTitle, SheetTrigger } from "./ui/sheet";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { use, useEffect } from "react";
 import { Avatar, AvatarImage } from "./ui/avatar";
+import Link from "next/link";
 
 const MenuSheet = () => {
   const handleLoginClick = async () => {
@@ -39,39 +40,52 @@ const MenuSheet = () => {
               variant={"secondary"}
               className="w-full justify-start text-xs"
             >
-              <LogInIcon size={18} className="mr-2"/>
+              <LogInIcon size={18} className="mr-2" />
               Fazer Login
             </Button>
           </div>
         )}
         {data?.user ? (
           <div className="text-sm w-full py-5 ">
-            <div className="flex items-center gap-3">
-              <Avatar>
-                <AvatarImage src={data.user?.image ?? ""} />
-              </Avatar>
-              Olá {data.user?.name}
-              <LogInIcon onClick={handleLogOutClick} />
+            <div className="flex items-center gap-3 justify-between">
+              <div className=" flex items-center gap-3">
+                <Avatar>
+                  <AvatarImage src={data.user?.image ?? ""} />
+                </Avatar>
+                {data.user?.name}
+              </div>
+              <Button variant={"secondary"} size={"icon"}>
+                <LogInIcon onClick={handleLogOutClick} />
+              </Button>
             </div>
           </div>
         ) : (
           <></>
         )}
-        <div className="w-full pt-3 flex">
-          <Button
-            variant={"secondary"}
-            className="w-full justify-start text-xs"
-          >
-            Inicio
-          </Button>
-        </div>
-        <div className="w-full pt-3 flex">
-          <Button
-            variant={"secondary"}
-            className="w-full justify-start text-xs"
-          >
-            Agendamentos
-          </Button>
+        <div className="w-full pt-3 flex flex-col gap-3 px-2" >
+          <div className="flex">
+            <Button
+              variant={"secondary"}
+              className="w-full justify-start text-xs flex" asChild
+            >
+              <Link href={"/"}>
+                <HomeIcon size={18} className="mr-2"/>
+                Agendamentos
+              </Link>
+            </Button>
+          </div>
+          {data?.user && (
+            <Button
+              variant={"secondary"}
+              className="w-full justify-start text-xs"
+              asChild
+            >
+              <Link href={"/bookings"}>
+                <CalendarIcon size={18} className="mr-2" />
+                Agendamentos
+              </Link>
+            </Button>
+          )}
         </div>
         {/* {status === "authenticated" && (
           <div className="w-full pt-3 flex">
