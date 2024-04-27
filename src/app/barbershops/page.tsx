2722dcc9473fb131fe3c3cbb536b4bff2201c7ba
Wander Amaral <wanderamaral013@gmail.com>
2024-04-27 14:09:05 -0300

    feat: ensure user can search for barbershop

---
+++ src/app/barbershops/page.tsx
@@ -0,0 +1,37 @@
+import BarbershopItem from "../(home)/_components/barbershop-item";
+import Header from "../_components/header";
+import { db } from "../_lib/prisma";
+
+interface BarbershopPageProps {
+  searchParams: {
+    search?: string;
+  };
+}
+
+const BarbershopPage = async ({ searchParams }: BarbershopPageProps) => {
+  const barbershops = await db.barbershop.findMany({
+    where: {
+      name: {
+        contains: searchParams.search,
+        mode: "insensitive",
+      },
+    },
+  });
+  return (
+    <>
+      <Header />
+      <div className="px-5 py-6">
+        <h1 className="text-sm font-bold text-gray-400 uppercase">
+          Resultados para &quot;{searchParams.search}&quot;
+        </h1>
+        <div className="grid grid-cols-2 mt-4 gap-3">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem barbershop={barbershop} key={barbershop.id} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BarbershopPage;
