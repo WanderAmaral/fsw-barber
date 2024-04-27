c201dbb22dd5572d7e0a336f8fdc7eca7df1fdb9
Wander Amaral <wanderamaral013@gmail.com>
2024-04-27 20:50:52 -0300

    fix: logo for text

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -7,8 +7,12 @@ const Header = () => {
   return (
     <Card>
       <CardContent className="py-6 px-5 flex items-center justify-between">
-        <Image src={"/logo.png"} alt="logo" height={22} width={130} />
-
+        <Link href={"/"}>
+          {/* <Image src={"/logo.png"} alt="logo" height={22} width={130} /> */}
+          <h1 className="font-bold text-2xl">
+            <span className="text-primary">FSW </span>BARBER
+          </h1>
+        </Link>
         <MenuSheet />
       </CardContent>
     </Card>
