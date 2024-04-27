9e1525eb6bcde583a259487cad41e477dfdd8c20
Wander Amaral <wanderamaral013@gmail.com>
2024-04-26 22:56:10 -0300

    commit

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,15 +1,14 @@
-/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import MenuSheet from "./menu-sheet";
 import Link from "next/link";
-  
+
 const Header = () => {
   return (
     <Card>
       <CardContent className="py-6 px-5 flex items-center justify-between">
         <Link href={"/"}>
-          <img src={"/logo.png"} alt="logo" height={22} width={130} />
+          <Image src={"/logo.png"} alt="logo" height={22} width={130} />
         </Link>
         <MenuSheet />
       </CardContent>
